fa2923c39deacbb2546cc8bc2e6eee4909ee7789
chaoukat99 <chawkatomar@gmail.com>
2025-05-04 13:08:33 +0100

    04/05/2025

---
+++ JS/main.js
@@ -0,0 +1,81 @@
+
+// Base 
+
+
+// variables / constantes
+
+
+let username="Karim" ;
+let firstname="Samir";
+username="Salim"
+
+
+console.log(username);
+
+
+// Constantes
+
+
+const PI=3.1466760;
+
+
+// Primitive
+// string
+
+let Username='Salimi99';
+
+
+// Number (int float double )
+let ageUser=16;
+let salary=20000.52
+
+// Boolean 
+
+let is_connected=false; // true
+// null
+
+
+let schoolName=null;
+
+
+// undefined
+
+
+// Single Line comment
+
+
+/*
+
+multiple 
+line 
+comment
+-data types
+-opérateurs 
+-control flow (conditions loops)
+
+-functions predefinis / custom
+
+
+
+
+
+
+
+
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+// DOM(DOCUMENT OBJECT MODEL)
+
+
+
